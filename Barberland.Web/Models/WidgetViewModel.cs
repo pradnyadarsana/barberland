c5c352b6ac24ed23df4015c9b6874b3110d8158f
pradnyadarsana <pradnyadarsana13@gmail.com>
2023-12-10 16:03:56 +0800

    create widget navigation bar & footer

---
+++ Barberland.Web/Models/WidgetViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Barberland.Web.Models
+{
+	public class WidgetViewModel
+	{
+		public WidgetViewModel()
+		{
+		}
+	}
+
+    public class NavigationBarViewModel
+    {
+    }
+
+    public class FooterViewModel
+    {
+        public required string FooterDescription { get; set; }
+        public required string FooterContactMobile { get; set; }
+        public required string FooterContactEmail { get; set; }
+        public required string FooterCopyright { get; set; }
+        public required string FacebookURL { get; set; }
+        public required string TwitterURL { get; set; }
+        public required string InstagramURL { get; set; }
+    }
+}
+
---
+++ Barberland.Web/ViewComponents/WidgetViewComponent.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Reflection.Metadata;
+using Barberland.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Barberland.Web.ViewComponents
+{
+	public class WidgetViewComponent : ViewComponent
+	{
+		public WidgetViewComponent()
+		{
+		}
+
+        public IViewComponentResult Invoke(string ViewName)
+        {
+            object ViewModel;
+            if (ViewName == "NavigationBar")
+            {
+                ViewModel = new NavigationBarViewModel();
+            }
+            else if (ViewName == "Footer")
+            {
+                ViewModel = PopulateFooterViewModel();
+            }
+            else
+            {
+                ViewModel = new object();
+            }
+            return View(ViewName, ViewModel);
+        }
+
+        private static FooterViewModel PopulateFooterViewModel()
+        {
+            FooterViewModel ViewModel = new()
+            {
+                FooterDescription = "Test Footer Description",
+                FooterContactMobile = "089123456789",
+                FooterContactEmail = "testemail@gmail.com",
+                FooterCopyright = "Barberland 2023",
+                FacebookURL = "https://facebook.com",
+                TwitterURL = "https://x.com",
+                InstagramURL = "https://instagram.com"
+            };
+
+            return ViewModel;
+        }
+    }
+}
+
